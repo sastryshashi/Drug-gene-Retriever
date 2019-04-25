afca7a97913d027a035588c19d72e00f44561623
Shashidhar <shashidsastry@gmail.com>
2019-04-24 21:37:55 -0500

    The search function now works. Also added a view for searchResults

--- app.js
+++ app.js
@@ -22,25 +22,38 @@ app.get ("/", (req, res) => {
 //Search results page
 app.get ("/searchResults", (req, res) => {
   var searchTerm = req.query.searchTerm;
-  var apiPath = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=" + searchTerm + "+gene+pharmaceutical" + "&retmax=20";
+  var apiPath = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=" + searchTerm + "+gene+pharmaceutical" + "&retmax=2";
   request (apiPath, (err, response, body) => {
     if (err) {
         res.send ("There was an error");
         console.log (err);
     } else if (response.statusCode == 200) {
-        var jsonString, json, idElements, ids = [];
-        jsonString = convert.xml2json(body);
-        json = JSON.parse(jsonString);
-        idElements = json.elements[1].elements[3].elements;
-        //try using the map function here
-        idElements.forEach((element) => {
-          ids.push(element.elements[0].text);
-        });
-        res.send(ids);
+        ids = extractIDs(body);
+        getDataFromIDs (res, ids);
     }
   });
 });
 
+function extractIDs (body) {
+  var jsonString, json, idElements, ids = [];
+  jsonString = convert.xml2json(body);
+  json = JSON.parse(jsonString);
+  idElements = json.elements[1].elements[3].elements;
+  //try using the map function here
+  idElements.forEach((element) => {
+    ids.push(element.elements[0].text);
+  });
+  return ids;
+}
+
+function getDataFromIDs (res, ids) {
+  let promises = ids.map(id => getArticleDetails(id));
+  Promise.all(promises)
+  .then(values => {
+    res.render("searchResults", {values: values});
+  })
+  .catch (err => console.error ("Error: ", err));
+}
 
 /*
 This is a temporary function to test retrieval of {title: title, abstract: abstract} from getArticleDetails(id).
@@ -48,7 +61,7 @@ This is a temporary function to test retrieval of {title: title, abstract: abstr
 app.get ("/idsTest", (req, res) => {
   // var ids = ["30145211", "30128536", "30086764", "30079159", "30015382", "30006610", "29976630", "29973717", "29873142", "29844838", "29785153",
   //            "29731985", "29727754", "29713086", "29649003", "29580810", "29559847", "29545475", "29414022", "29214031"];
-  var ids = ["30145211", "30128536", "30086764"];
+  var ids = ['31011939', '31011918'];
   let promises = ids.map(id => getArticleDetails(id));
   Promise.all(promises)
   .then(values => {
@@ -80,9 +93,9 @@ async function getArticleDetails (id) {
       title = (doc.documentElement.getElementsByTagName("ArticleTitle")[0].childNodes[0].data);
     })
     .catch ((err) => {
-      console.log ("Could not retrieve something for" + id);
+      console.log ("Could not retrieve something for " + id);
     });
-  return {title: title, abstract: abstract, id: id};
+  return {title: title, abstract: abstract};
 }
 
 
